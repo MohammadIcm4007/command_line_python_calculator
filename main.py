e89c126a73aa954ca26cbbe9b11d28ac4a452362
mmsh <mohammadmehdisharife@gmail.com>
2024-10-23 00:21:01 +0330

    primary base

---
+++ main.py
@@ -0,0 +1,9 @@
+# welcome & get inputs
+print("<< welcome to the calculator >>")
+print("enter your first number :", end=" ")
+num_1 = input()
+print("enter your operation :", end=" ")
+operation = input()
+print("enter your secend number :", end=" ")
+num_2 = input()
+# process 
